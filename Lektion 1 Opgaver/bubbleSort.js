75efb5c726624dbceea34cb32f39212b5ba02a06
Frederik Sloth Stræde <frederiksloths@gmail.com>
2019-02-01 11:29:21 +0100

    3

--- Lektion 1 Opgaver/bubbleSort.js
+++ Lektion 1 Opgaver/bubbleSort.js
@@ -10,4 +10,24 @@ for (let i = list.length - 1; i >= 0; i--) {
         }
     }
 }
+
 console.log(list); // => 0,1,2,4,7,8,9,13,16
+
+// opgave 1.3
+function binarySearch(arr, i) {
+    var mid = Math.floor(arr.length / 2);
+
+
+    if (arr[mid] === i) {
+        return arr[mid];
+    } else if (arr[mid] < i && arr.length > 1) {
+        return binarySearch(arr.splice(mid, Number.MAX_VALUE), i);
+    } else if (arr[mid] > i && arr.length > 1) {
+        return binarySearch(arr.splice(0, mid), i);
+    } else {
+        console.log('The input', i, "is not here");
+        return -1;
+    }
+}
+
+console.log(binarySearch(list, 7))
--- Lektion 1 Opgaver/bubbleSortString.js
+++ Lektion 1 Opgaver/bubbleSortString.js
@@ -1,4 +1,4 @@
-// Opgave 1.1 
+// Opgave 1.2
 let list = ["Xalia", "Frederik", "Søren", "Erik", "Abe"]
 
 for (let i = list.length - 1; i >= 0; i--) {
@@ -11,4 +11,24 @@ for (let i = list.length - 1; i >= 0; i--) {
     }
 }
 
+
 console.log(list);
+
+//Opgave 1.3
+function binarySearch(arr, i) {
+    var mid = Math.floor(arr.length / 2);
+
+
+    if (arr[mid] === i) {
+        return arr[mid];
+    } else if (arr[mid] < i && arr.length > 1) {
+        return binarySearch(arr.splice(mid, Number.MAX_VALUE), i);
+    } else if (arr[mid] > i && arr.length > 1) {
+        return binarySearch(arr.splice(0, mid), i);
+    } else {
+        console.log('The input', i, "is not here");
+        return -1;
+    }
+}
+
+console.log(binarySearch(list, "U"))
